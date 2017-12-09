517b0e2ecf1027d944144264e821b91e962f5cc0
konyekokim <34070156+konyekokim@users.noreply.github.com>
2017-12-09 15:03:45 +0100

    rounded up skeleton of the get started activity and creating user authentication activities

--- app/src/main/java/com/chokus/konye/packmyload/SignUpActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/SignUpActivity.kt
@@ -2,11 +2,18 @@ package com.chokus.konye.packmyload
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_pmltype)
+        setFullScreen()
+    }
+
+    private fun setFullScreen() {
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
