39a669942324e56a00df3961d1cdca10cc21f1aa
konyekokim <34070156+konyekokim@users.noreply.github.com>
2017-12-09 12:55:34 +0100

    Adding the GetStartedActivity

--- app/src/main/java/com/chokus/konye/packmyload/SplashScreenActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/SplashScreenActivity.kt
@@ -1,12 +1,28 @@
 package com.chokus.konye.packmyload
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        setFullScreen()
+        val WELCOME_TIMEOUT = 4000
+        Handler().postDelayed({
+            val intent = Intent(applicationContext, GetStartedActivity::class.java)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            startActivity(intent)
+            finish()
+        }, WELCOME_TIMEOUT.toLong())
+    }
+
+    private fun setFullScreen() {
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
