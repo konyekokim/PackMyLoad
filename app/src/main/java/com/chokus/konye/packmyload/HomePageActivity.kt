17cab23850127e703f5e01ec161bae6337508a11
konyekokim <34070156+konyekokim@users.noreply.github.com>
2018-01-11 09:58:13 +0100

    editing the pml home page

--- app/src/main/java/com/chokus/konye/packmyload/HomePageActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/HomePageActivity.kt
@@ -2,11 +2,14 @@ package com.chokus.konye.packmyload
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class HomePageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        supportActionBar!!.hide()
     }
+
 }
