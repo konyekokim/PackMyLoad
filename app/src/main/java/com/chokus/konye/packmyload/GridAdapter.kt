29c7043102d733177a2bc84c71a114a6d4265eb2
konyekokim <34070156+konyekokim@users.noreply.github.com>
2018-01-11 11:25:18 +0100

    added grid item and grid adapter

---
+++ app/src/main/java/com/chokus/konye/packmyload/GridAdapter.kt
@@ -0,0 +1,28 @@
+package com.chokus.konye.packmyload
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+/**
+ * Created by ALPHA AND JAM on 1/11/2018.
+ */
+class GridAdapter(context: Context, serviceImg: Int, serviceName: String) : BaseAdapter(){
+    override fun getItem(p0: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
