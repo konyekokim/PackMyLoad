c6ec673e1daa3559e3243e3f8a3c88297bd4464e
konyekokim <34070156+konyekokim@users.noreply.github.com>
2018-01-12 11:51:27 +0100

    completed the drawerlayout skeleton remaining to design and fill in icons and also removing gridView from inside scrollview

--- app/src/main/java/com/chokus/konye/packmyload/HomePageActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/HomePageActivity.kt
@@ -2,6 +2,7 @@ package com.chokus.konye.packmyload
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,22 +12,41 @@ import android.view.WindowManager
 import android.widget.BaseAdapter
 import android.widget.GridView
 import kotlinx.android.synthetic.main.activity_home_page.*
+import kotlinx.android.synthetic.main.drawer_list_item.view.*
 import kotlinx.android.synthetic.main.grid_item.view.*
 
 class HomePageActivity : AppCompatActivity() {
-    var adapter : ServiceGridAdapter? = null
+    var gridAdapter : ServiceGridAdapter? = null
+    var listAdapter : DrawerListAdapter? = null
     var serviceList= ArrayList<ServiceClass>()
+    var iconList = ArrayList<ServiceClass>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
         supportActionBar!!.hide()
+        viewActions()
+        gridAdapter = ServiceGridAdapter(this, serviceList)
+        grid_view.adapter = gridAdapter
 
-        adapter = ServiceGridAdapter(this, serviceList)
-        grid_view.adapter = adapter
+        listAdapter = DrawerListAdapter(this, iconList)
+        left_drawer_listView.adapter = listAdapter
+    }
+
+    private fun viewActions(){
+        pml_logo.setOnClickListener {
+            pml_drawer_layout.openDrawer(home_page_drawer_layout)
+        }
+        gift_img.setOnClickListener {
+            //here pass intent to the gift activity
+        }
     }
 
     private fun addGridContent(){
         //here perform serviceList.add when you are ready
+    }
+
+    private fun addListContent(){
+        //here perform iconList.add when you are ready
     }
 
     class ServiceGridAdapter : BaseAdapter{
@@ -59,6 +79,39 @@ class HomePageActivity : AppCompatActivity() {
             serviceGridView.service_img.setImageResource(service.img!!)
 
             return serviceGridView
+        }
+    }
+
+    class DrawerListAdapter : BaseAdapter{
+        var iconList = ArrayList<ServiceClass>()
+        var context: Context
+
+        constructor(context: Context, iconList: ArrayList<ServiceClass>){
+            this.iconList = iconList
+            this.context = context
+        }
+
+        override fun getCount(): Int {
+            return iconList.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return iconList[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        @SuppressLint("ViewHolder")
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val icon = this.iconList[p0]
+            val inflator = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val iconListView = inflator.inflate(R.layout.drawer_list_item,null)
+            iconListView.icon_imageView.setImageResource(icon.img!!)
+            iconListView.icon_nameTextView.text = icon.name
+
+            return iconListView
         }
 
     }
