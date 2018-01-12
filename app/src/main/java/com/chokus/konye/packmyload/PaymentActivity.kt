a1d119612637b643565590bebad67fb25c563e58
konyekokim <34070156+konyekokim@users.noreply.github.com>
2018-01-12 20:26:34 +0100

    added the payment activity remaining to tweak it

---
+++ app/src/main/java/com/chokus/konye/packmyload/PaymentActivity.kt
@@ -0,0 +1,35 @@
+package com.chokus.konye.packmyload
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_payment.*
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        setTitle(R.string.payment_title)
+    }
+
+    private fun viewActions(){
+        add_card_relativeLayout.setOnClickListener {
+            //pass intent here to the empty add card activity to add new card
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_edit_payment,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item!!.itemId == R.id.edit){
+            //pass intent from here to the add card Activity with details of former saved card
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
