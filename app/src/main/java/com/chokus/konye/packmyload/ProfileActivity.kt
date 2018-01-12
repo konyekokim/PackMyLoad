5cca24376567cbf608a3aa9559ab979dff8601b1
konyekokim <34070156+konyekokim@users.noreply.github.com>
2018-01-12 19:27:19 +0100

    added the profile activity remaining to tweak it

---
+++ app/src/main/java/com/chokus/konye/packmyload/ProfileActivity.kt
@@ -0,0 +1,28 @@
+package com.chokus.konye.packmyload
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        setTitle(R.string.profile_title)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save_profile,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item!!.itemId == R.id.save){
+            //do saving action here wait for backend to be ready and collect from widgets here
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
