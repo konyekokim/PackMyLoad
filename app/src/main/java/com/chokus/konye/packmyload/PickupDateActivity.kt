a295387949c05d821a829a48e873cf14a5393514
Konye <konyekokim@gmail.com>
2018-02-10 10:10:45 +0100

    added a few intents to the pickupdate Activity

--- app/src/main/java/com/chokus/konye/packmyload/PickupDateActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/PickupDateActivity.kt
@@ -1,12 +1,28 @@
 package com.chokus.konye.packmyload
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pickup_date.*
 
 class PickupDateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pickup_date)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        setTitle(R.string.date_pickup_title_example)
+        viewActions()
+    }
+
+    private fun viewActions(){
+        add_payment_method_layout.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            startActivity(intent)
+        }
+        add_payment_relativeLayout.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
