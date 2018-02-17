b2e23eaac060b32911fcc00dc92acd3bbe3ca819
Konye <konyekokim@gmail.com>
2018-02-18 00:36:57 +0100

    added the date picker dialog

--- app/src/main/java/com/chokus/konye/packmyload/SelectPickupTimeActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/SelectPickupTimeActivity.kt
@@ -1,11 +1,17 @@
 package com.chokus.konye.packmyload
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
 import kotlinx.android.synthetic.main.activity_select_pickup_time.*
+import java.text.SimpleDateFormat
+import java.time.Year
+import java.util.*
 
 class SelectPickupTimeActivity : AppCompatActivity() {
+    private var calendar : Calendar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +19,7 @@ class SelectPickupTimeActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.select_pickup_time)
         viewActions()
+        calendar = Calendar.getInstance()
     }
 
     private fun viewActions(){
@@ -20,5 +27,21 @@ class SelectPickupTimeActivity : AppCompatActivity() {
             val intent = Intent(this, ItemsMovingActivity::class.java)
             startActivity(intent)
         }
+        choose_date_layout.setOnClickListener {
+            //calender function here
+            datePickerAction()
+        }
+    }
+
+    private fun datePickerAction(){
+        val date = object: DatePickerDialog.OnDateSetListener{
+            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                calendar!!.set(Calendar.YEAR,year)
+                calendar!!.set(Calendar.MONTH,month)
+                calendar!!.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+                chosen_date_textView.text = SimpleDateFormat("MMM-d", Locale.ENGLISH).format(calendar!!.time)
+            }
+        }
+        DatePickerDialog(this,R.style.DialogTheme, date,calendar!!.get(Calendar.YEAR), calendar!!.get(Calendar.MONTH), calendar!!.get(Calendar.DAY_OF_MONTH)).show()
     }
 }
