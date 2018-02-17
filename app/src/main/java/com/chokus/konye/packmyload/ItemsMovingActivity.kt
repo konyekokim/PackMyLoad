12484c15095fdeda04b9856eeb6da987b3a2944d
Konye <konyekokim@gmail.com>
2018-02-17 18:06:53 +0100

    added intent from itemsMovingActivity to PickUpDateActivity

--- app/src/main/java/com/chokus/konye/packmyload/ItemsMovingActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/ItemsMovingActivity.kt
@@ -1,7 +1,9 @@
 package com.chokus.konye.packmyload
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_items_moving.*
 
 class ItemsMovingActivity : AppCompatActivity() {
 
@@ -10,5 +12,16 @@ class ItemsMovingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_items_moving)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.items_moving_activity)
+        viewActions()
+    }
+
+    private fun viewActions(){
+        camera_icon.setOnClickListener {
+            //add needed action here
+        }
+        add_photo_layout.setOnClickListener {
+            val intent = Intent(this, PickupDateActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
