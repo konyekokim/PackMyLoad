888b582f42c329ff22bc8b0721ca30d749e5d5f5
Konye <konyekokim@gmail.com>
2018-02-20 03:26:20 +0100

    added my applicaton name and also the facebook fresco api

---
+++ app/src/main/java/com/chokus/konye/packmyload/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.chokus.konye.packmyload
+
+import android.app.Application
+import android.support.multidex.MultiDex
+import com.facebook.drawee.backends.pipeline.Fresco
+
+/**
+ * Created by omen on 20/02/2018.
+ */
+class MyApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        MultiDex.install(this)
+
+        Fresco.initialize(this)
+    }
+}
