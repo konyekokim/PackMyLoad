dcbd83102d038d6907453003ce8315b6fa434c93
Konye <konyekokim@gmail.com>
2018-04-03 11:33:20 +0100

    edit myapplication class to be volley singleton class

--- app/src/main/java/com/chokus/konye/packmyload/application/MyApplication.kt
+++ app/src/main/java/com/chokus/konye/packmyload/application/MyApplication.kt
@@ -3,6 +3,10 @@ package com.chokus.konye.packmyload.application
 import android.content.Context
 import android.support.multidex.MultiDex
 import android.support.multidex.MultiDexApplication
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.VolleyLog.TAG
+import com.android.volley.toolbox.Volley
 import com.facebook.FacebookSdk
 import com.facebook.appevents.AppEventsLogger
 import com.facebook.drawee.backends.pipeline.Fresco
@@ -20,6 +24,28 @@ class MyApplication : MultiDexApplication(){
         FacebookSdk.sdkInitialize(applicationContext)
         AppEventsLogger.activateApp(this)
         FirebaseApp.initializeApp(this)
+        //adding volley singleton instance
+        instance = this
+    }
+
+    val requestQueue : RequestQueue? = null
+
+    get(){
+        if(field == null){
+            return Volley.newRequestQueue(applicationContext)
+        }
+        return field
+    }
+
+    fun <T> addToRequestQueue(request : Request<T>){
+        request.tag = TAG
+        requestQueue?.add(request)
+    }
+
+    companion object {
+        private val TAG = MyApplication::class.java.simpleName
+        @get:Synchronized var instance : MyApplication? = null
+        private set
     }
 
     override fun attachBaseContext(base: Context?) {
