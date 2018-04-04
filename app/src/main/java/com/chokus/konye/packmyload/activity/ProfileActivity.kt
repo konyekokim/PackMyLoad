bd2d13c3d095def42d0af3e219f1501ce7be28b7
Konye <konyekokim@gmail.com>
2018-04-04 12:55:01 +0100

    add condition and flow for  implementation of volley in the application

--- app/src/main/java/com/chokus/konye/packmyload/activity/ProfileActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/activity/ProfileActivity.kt
@@ -1,18 +1,44 @@
 package com.chokus.konye.packmyload.activity
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.chokus.konye.packmyload.R
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    private var progressDialog : ProgressDialog? = null
+    private val URL ="put in the valid URLs here"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.profile_title)
+        progressDialog = ProgressDialog(this)
+    }
+
+    private fun onViewActions(){
+        //checking if edit text widgets are empty or not
+        if(firstName_editText.toString().isEmpty()){
+            Toast.makeText(this, "Please enter your fist naeme", Toast.LENGTH_SHORT).show()
+        }
+        if(lastName_editText.toString().isEmpty()){
+            Toast.makeText(this, "Plesse enter your last name", Toast.LENGTH_SHORT).show()
+        }
+        if(emailAddress_editText.toString().isEmpty()){
+            Toast.makeText(this, "Plesse enter your last name", Toast.LENGTH_SHORT).show()
+        }
+        if(phoneNumber_editText.toString().isEmpty()){
+            Toast.makeText(this, "Please enter your phone number", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -25,5 +51,11 @@ class ProfileActivity : AppCompatActivity() {
             //do saving action here wait for backend to be ready and collect from widgets here
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun sendData(){
+        progressDialog!!.setMessage("Saving data please wait...")
+        progressDialog!!.show()
+
     }
 }
