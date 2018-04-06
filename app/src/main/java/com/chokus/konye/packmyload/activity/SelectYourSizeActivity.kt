081e04518e07c91e3b523ff049e3f93cb5a2f0da
Konye <konyekokim@gmail.com>
2018-04-06 12:32:30 +0100

    implement volley sendData method into SelectYourSizeActivity

--- app/src/main/java/com/chokus/konye/packmyload/activity/SelectYourSizeActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/activity/SelectYourSizeActivity.kt
@@ -5,8 +5,14 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
 import com.chokus.konye.packmyload.R
+import com.chokus.konye.packmyload.application.MyApplication
 import kotlinx.android.synthetic.main.activity_select_your_size.*
+import org.json.JSONObject
 
 class SelectYourSizeActivity : AppCompatActivity() {
     private var progressDialog : ProgressDialog? = null
@@ -23,9 +29,38 @@ class SelectYourSizeActivity : AppCompatActivity() {
 
     private fun viewActions(){
         continue_layout.setOnClickListener {
-            val intent = Intent(this, SelectPickupTimeActivity::class.java)
-            startActivity(intent)
+           sendData()
         }
+    }
+
+    private fun sendData(){
+        progressDialog!!.setMessage("Loading")
+        progressDialog!!.show()
+        val stringRequest = object : StringRequest(Request.Method.POST, URL,
+                Response.Listener<String>{ response ->
+                    //use this to get hte response from the backend
+                    progressDialog!!.dismiss()
+                    val obj = JSONObject(response)
+                    //Toast.makeText(applicationContext, obj.getString("what ever the string return " +
+                    //        "in backend"), Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, SelectPickupTimeActivity::class.java)
+                    startActivity(intent)
+
+                }, object : Response.ErrorListener{
+            override fun onErrorResponse(error: VolleyError?) {
+                progressDialog!!.dismiss()
+                toastMethod(error?.message)
+            }
+        })
+        {
+            override fun getParams(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                //need better API to add the information from this activity to the database e.g. shown  below
+                //params.put("name", firstName_editText.text.toString())
+                return super.getParams()
+            }
+        }
+        MyApplication.instance?.addToRequestQueue(stringRequest)
     }
 
     private fun toastMethod(message : String?){
