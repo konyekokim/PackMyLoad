40a6474beeec673fa8ed9902c6ae4757b2ad2879
Konye <konyekokim@gmail.com>
2018-04-10 16:29:32 +0100

    add  network checking  function to selectPickuptime activity

--- app/src/main/java/com/chokus/konye/packmyload/activity/SelectPickupTimeActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/activity/SelectPickupTimeActivity.kt
@@ -3,11 +3,16 @@ package com.chokus.konye.packmyload.activity
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.app.ProgressDialog
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.widget.ArrayAdapter
 import android.widget.DatePicker
+import android.widget.RelativeLayout
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
@@ -32,6 +37,7 @@ class SelectPickupTimeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_select_pickup_time)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.select_pickup_time)
+        checkNetworkConnection()
         viewActions()
         calendar = Calendar.getInstance()
         progressDialog = ProgressDialog(this)
@@ -125,6 +131,25 @@ class SelectPickupTimeActivity : AppCompatActivity() {
             }
         }
         MyApplication.instance?.addToRequestQueue(stringRequest)
+    }
+
+    private fun checkNetworkConnection() {
+        val backgroundLayout = findViewById(R.id.select_pickup_time_activity_layout) as RelativeLayout
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).state == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).state == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            Snackbar.make(backgroundLayout, "Connection successful", Snackbar.LENGTH_SHORT).show()
+        } else {
+            //we are not connected to a network
+            Snackbar.make(backgroundLayout, "Oops! No internet connection", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("RETRY") {
+                        val intent = intent
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        startActivity(intent)
+                        finish()
+                    }.setActionTextColor(resources.getColor(R.color.colorPrimary)).show()
+        }
     }
 
     private fun toastMethod(message : String?){
