3333b9d23799a324f6b5c8d78941044cb926832d
Konye <konyekokim@gmail.com>
2018-04-10 15:03:45 +0100

    add network  availability checker to about activity

--- app/src/main/java/com/chokus/konye/packmyload/activity/AboutActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/activity/AboutActivity.kt
@@ -1,7 +1,15 @@
 package com.chokus.konye.packmyload.activity
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.widget.DrawerLayout
+import android.widget.RelativeLayout
 import com.chokus.konye.packmyload.R
 
 class AboutActivity : AppCompatActivity() {
@@ -11,5 +19,25 @@ class AboutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_about)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.about_title)
+        checkNetworkConnection()
+    }
+
+    private fun checkNetworkConnection() {
+        val backgroundLayout = findViewById(R.id.about_activity_layout) as RelativeLayout
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).state == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).state == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            Snackbar.make(backgroundLayout, "Connection successful", Snackbar.LENGTH_SHORT).show()
+        } else {
+            //we are not connected to a network
+            Snackbar.make(backgroundLayout, "Oops! No internet connection", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("RETRY") {
+                        val intent = intent
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        startActivity(intent)
+                        finish()
+                    }.setActionTextColor(Color.parseColor("#FC7900")).show()
+        }
     }
 }
