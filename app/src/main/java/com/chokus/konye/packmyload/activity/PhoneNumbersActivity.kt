6c108554cdbe7cff8fdce3d036310ebdd8b235fc
Konye <konyekokim@gmail.com>
2018-04-10 16:13:29 +0100

    add  network checking  function to phoneNumber activity

--- app/src/main/java/com/chokus/konye/packmyload/activity/PhoneNumbersActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/activity/PhoneNumbersActivity.kt
@@ -1,11 +1,16 @@
 package com.chokus.konye.packmyload.activity
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.Toast
 import com.chokus.konye.packmyload.R
 import com.google.firebase.FirebaseException
@@ -31,6 +36,7 @@ class PhoneNumbersActivity : AppCompatActivity() {
         setContentView(R.layout.activity_phone_numbers)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.phone_numb)
+        checkNetworkConnection()
         viewActions()
         mCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
@@ -139,9 +145,27 @@ class PhoneNumbersActivity : AppCompatActivity() {
                 token)             // ForceResendingToken from callbacks
     }
 
+    private fun checkNetworkConnection() {
+        val backgroundLayout = findViewById(R.id.phone_number_activity_layout) as RelativeLayout
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).state == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).state == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            Snackbar.make(backgroundLayout, "Connection successful", Snackbar.LENGTH_SHORT).show()
+        } else {
+            //we are not connected to a network
+            Snackbar.make(backgroundLayout, "Oops! No internet connection", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("RETRY") {
+                        val intent = intent
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        startActivity(intent)
+                        finish()
+                    }.setActionTextColor(resources.getColor(R.color.colorPrimary)).show()
+        }
+    }
+
     private fun showToast(toastText : String){
         Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show()
-
     }
 
 }
