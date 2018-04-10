cdcf5b0a9559a0ce90768f1dfa2512c80d03c4d9
Konye <konyekokim@gmail.com>
2018-04-10 16:09:42 +0100

    add  network checking  function to payment activity

--- app/src/main/java/com/chokus/konye/packmyload/activity/PaymentActivity.kt
+++ app/src/main/java/com/chokus/konye/packmyload/activity/PaymentActivity.kt
@@ -1,10 +1,15 @@
 package com.chokus.konye.packmyload.activity
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.RelativeLayout
 import com.chokus.konye.packmyload.R
 import kotlinx.android.synthetic.main.activity_payment.*
 
@@ -15,6 +20,7 @@ class PaymentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_payment)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.payment_title)
+        checkNetworkConnection()
         viewActions()
     }
 
@@ -35,5 +41,24 @@ class PaymentActivity : AppCompatActivity() {
             //pass intent from here to the add card Activity with details of former saved card
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun checkNetworkConnection() {
+        val backgroundLayout = findViewById(R.id.payment_activity_layout) as RelativeLayout
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).state == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).state == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            Snackbar.make(backgroundLayout, "Connection successful", Snackbar.LENGTH_SHORT).show()
+        } else {
+            //we are not connected to a network
+            Snackbar.make(backgroundLayout, "Oops! No internet connection", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("RETRY") {
+                        val intent = intent
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        startActivity(intent)
+                        finish()
+                    }.setActionTextColor(resources.getColor(R.color.colorPrimary)).show()
+        }
     }
 }
